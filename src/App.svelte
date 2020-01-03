d6adae3c655af87bbcb7dec8c6140f1e9cf2ad6f
Gaurav Raj Sharma <blazegaurav123@gmail.com>
2020-01-03 20:51:44 +0545

    - commit it

--- src/App.svelte
+++ src/App.svelte
@@ -5,4 +5,5 @@
 <div class="container p-3">
   <span class="text-indigo-800 font-bold text-4xl shadow p-4">{heading}</span>
   <span>Understanding webhooks</span>
+  <span>Webhooks let's you do cool things</span>
 </div>
